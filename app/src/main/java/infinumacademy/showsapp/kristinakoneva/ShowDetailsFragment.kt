9f5a74c46b8d2254497ea5e08a5d6bc717a6a5fc
Kristina Koneva <koneva.kristinaa@gmail.com>
2022-07-20 12:59:48 +0200

    Create a fragment for each activity from assignment3

---
+++ app/src/main/java/infinumacademy/showsapp/kristinakoneva/ShowDetailsFragment.kt
@@ -0,0 +1,26 @@
+package infinumacademy.showsapp.kristinakoneva
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import infinumacademy.showsapp.kristinakoneva.databinding.FragmentShowDetailsBinding
+
+
+class ShowDetailsFragment : Fragment(){
+
+    private var _binding: FragmentShowDetailsBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding =  FragmentShowDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
