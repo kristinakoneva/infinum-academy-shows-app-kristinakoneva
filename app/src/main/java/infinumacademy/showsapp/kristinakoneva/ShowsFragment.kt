76fd04e3bc0204c63f78fbddca93a66ac9c1a5aa
Kristina Koneva <koneva.kristinaa@gmail.com>
2022-07-20 14:14:08 +0200

    Setup LoginFragment

--- app/src/main/java/infinumacademy/showsapp/kristinakoneva/ShowsFragment.kt
+++ app/src/main/java/infinumacademy/showsapp/kristinakoneva/ShowsFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
 import infinumacademy.showsapp.kristinakoneva.databinding.FragmentShowDetailsBinding
 import infinumacademy.showsapp.kristinakoneva.databinding.FragmentShowsBinding
 
@@ -14,6 +15,8 @@ class ShowsFragment : Fragment() {
     private var _binding:FragmentShowsBinding? = null
 
     private val binding get() = _binding!!
+
+    private val args by navArgs<ShowsFragmentArgs>()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding =  FragmentShowsBinding.inflate(inflater, container, false)
