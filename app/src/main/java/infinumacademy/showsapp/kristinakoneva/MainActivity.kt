0c9a6f727e8d5d23bf85969cacc3726a295511ed
Kristina Koneva <koneva.kristinaa@gmail.com>
2022-08-03 00:48:57 +0200

    Add a splash screen

--- app/src/main/java/infinumacademy/showsapp/kristinakoneva/MainActivity.kt
+++ app/src/main/java/infinumacademy/showsapp/kristinakoneva/MainActivity.kt
@@ -3,15 +3,16 @@ package infinumacademy.showsapp.kristinakoneva
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import infinumacademy.showsapp.kristinakoneva.databinding.ActivityMainBinding
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
     }
 }
