dec9955d0fe9d3c06183f4d3019048ea2024f5cb
Kristina Koneva <koneva.kristinaa@gmail.com>
2022-08-04 04:31:18 +0200

    Add more animations between fragments, add new app icon, update splash screen

--- app/src/main/java/infinumacademy/showsapp/kristinakoneva/MainActivity.kt
+++ app/src/main/java/infinumacademy/showsapp/kristinakoneva/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        val splashScreen = installSplashScreen()
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
